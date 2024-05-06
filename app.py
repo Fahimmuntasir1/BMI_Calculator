bb3103cde5ae8ba81876c86a71028e89936ed638
Fahimmuntasir1 <fahimmuntasir043@gamil.com>
2024-05-06 01:03:46 -0700

    bmi formuls added

--- app.py
+++ app.py
@@ -1,1 +1,3 @@
 # Create a BMI calculator to get BMI
+# Formula:
+# The formula is BMI = kg/m2 where kg is a person's weight in kilograms and m2 is their height in metres squared. A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9.
