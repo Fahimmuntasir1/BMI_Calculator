e832b7a44bbc3b88bef0b56389cb6ba56dce848c
Fahimmuntasir1 <fahimmuntasir043@gamil.com>
2024-05-07 01:04:29 -0700

    bmi calculator is working

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 # Create a BMI calculator to get BMI
 # Formula:
 # The formula is BMI = kg/m2 where kg is a person's weight in kilograms and m2 is their height in metres squared. A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9.
+
+hInFeet = float(input("Enter your height (Feet) : "))
+# convert feet into meter
+hInMeter = hInFeet * 0.3048
+
+weight = float(input("Enter your weight (KG) : "))
+
+bmi = weight / (hInMeter * 2)
+print(bmi)
